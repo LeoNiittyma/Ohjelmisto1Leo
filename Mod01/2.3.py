cc14033cb910b2cab2e8c685b22c5f4bffe549d8
Leo <leo.niittymaa@metropolia.fi>
2025-08-19 15:09:12 +0300

    tehtävät

---
+++ Mod01/2.3.py
@@ -0,0 +1,9 @@
+kanta_str = input("Suorakulmion kanta")
+korkeus_str = input("Suorakulmion korkeus")
+
+kanta = float(kanta_str)
+korkeus = float(korkeus_str)
+
+piiri = kanta*2 + korkeus*2
+
+print("Suorakulmion piiri on",piiri,"cm, kun kanta on",kanta,"cm ja piiri",piiri,"cm.")
