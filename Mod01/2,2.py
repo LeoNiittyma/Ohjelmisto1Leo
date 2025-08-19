31f37450c60e3da2b12434204d5d64ff570726b9
Leo <leo.niittymaa@metropolia.fi>
2025-08-19 15:02:07 +0300

    tehtävät

--- Mod01/2,2.py
+++ Mod01/2,2.py
@@ -1,6 +1,4 @@
 sade_str = input("Ympyrän säde: ")
 sade = float(sade_str)
-
 pintaala = sade**2*3.14
-
-print("Ympyrän pinta-ala on noin", pintaala, "cm, kun ympyrän säde on",sade,"cm.")
+print("Ympyrän pinta-ala on noin", pintaala, "cm, kun ympyrän säde on",sade,"cm. ")
