1b10112a87eb5604c83bff2fb4bc8c66a647543f
Leo <leo.niittymaa@metropolia.fi>
2025-08-20 09:29:06 +0300

    tehtävät

---
+++ Mod01/2.6.py
@@ -0,0 +1,18 @@
+import random
+
+random.randint(0,9)
+
+numero1 = random.randint(0,9)
+numero2 = random.randint(0,9)
+numero3 = random.randint(0,9)
+
+print(numero1,numero2,numero3)
+
+random.randint(1,6)
+
+numero2_1 = random.randint(1,6)
+numero2_2 = random.randint(1,6)
+numero2_3 = random.randint(1,6)
+numero2_4 = random.randint(1,6)
+
+print(numero2_1,numero2_2,numero2_3,numero2_4)
