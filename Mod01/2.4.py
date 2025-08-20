a6c76e80113c006eae5a309177c5ef1866293640
Leo <leo.niittymaa@metropolia.fi>
2025-08-20 09:11:26 +0300

    tehtävät

---
+++ Mod01/2.4.py
@@ -0,0 +1,18 @@
+luku1_str = input("Anna ensimmäinen kokonaisluku: ")
+luku2_str = input("Anna toinen kokonaisluku: ")
+luku3_str = input("Anna kolmas kokonaisluku: ")
+
+luku1 = float(luku1_str)
+luku2 = float(luku2_str)
+luku3 = float(luku3_str)
+
+summa = (luku1 + luku2 + luku3)
+tulo = (luku1 * luku2 * luku3)
+keskiarvo = ((luku1 + luku2 + luku3) / 3)
+
+
+print("Lukujen summa on",summa)
+print("Lukujen tulo on",tulo)
+print("Lukujen keskiarvoon",keskiarvo)
+
+
