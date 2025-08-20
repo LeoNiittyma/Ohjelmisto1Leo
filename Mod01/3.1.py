4122f50d200ab2fe8310d9933eecc499cc2e8322
Leo <leo.niittymaa@metropolia.fi>
2025-08-20 09:46:22 +0300

    tehtävät

---
+++ Mod01/3.1.py
@@ -0,0 +1,8 @@
+pituus = float(input("Kuhan pituus: "))
+
+if pituus < 37:
+    cm = 37 - pituus
+    print("Kuha on",cm,"cm alamittainen, laske se takaisin järveen!")
+
+else:
+    print("Kuha ei ole alimittainen! :)")
