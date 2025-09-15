0d3b99dfbbdd93dbbaec28af592baf6aecea050e
Leo <leo.niittymaa@metropolia.fi>
2025-09-15 15:48:16 +0300

    tehtävät

---
+++ 7.3.py
@@ -0,0 +1,19 @@
+lista = {}
+
+while True:
+    kysymys = input(
+    "Haluatko syöttää uuden lentoaseman, hakea jo syötetyistä lentoasemista vai lopettaa haun?: (Syöttää / Hakea / Lopettaa)"
+    )
+    if kysymys == "Syöttää":
+        uusi_icao = input("Anna lentoaseman ICAO-koodi: ")
+        uusi_nimi = input("Anna lentoaseman nimi: ")
+        lista[uusi_icao] = uusi_nimi
+    elif kysymys == "Hakea":
+        haku = input("Anna lentoaseman ICAO-koodi: ")
+        if haku in lista:
+            print(f"ICAO-koodia {haku} vastaavan lentokentän nimi on {lista[haku]}")
+    elif kysymys == "Lopettaa":
+        break
+    else:
+        print("Väärä syöte!")
+
